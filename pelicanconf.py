356dd2f9fea6c810d8d82f4caccc588364e426e7
David Talbot <david.talbot@gmail.com>
2015-11-16 12:39:33 +0000

    More adjusting of content to get things working with Markdown

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'David Talbot'
 SITENAME = u'David Talbot'
-SITEURL = ''
+SITEURL = 'http://niceguydave.com'
 
 PATH = 'content'
 
@@ -22,8 +22,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
